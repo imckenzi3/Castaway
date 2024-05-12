8aca03888efcec61585482ca1a1db33ca6f276ed
Isaiah-Mck <Mr.isaiahmck@gmail.com>
2024-05-11 18:13:58 -0700

    Updated Water Textures

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,28 +1,16 @@
 extends CharacterBody2D
 
+@export var speed = 100
+@export var accel = 10
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D as AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func _physics_process(_delta: float) -> void:
+		var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+		velocity.x = move_toward(velocity.x, speed * direction.x, accel)
+		velocity.y = move_toward(velocity.y, speed * direction.y, accel)
+		
+		move_and_slide()
+		
+#func _process(_delta: float) -> void:
---
+++ scripts/character.gd
@@ -0,0 +1,109 @@
+extends CharacterBody2D
+class_name Character
+
+#Hit effect
+#const HIT_EFFECT_SCENE: PackedScene = preload("res://characters/player/hitEffect.tscn")
+
+#player movement
+const FRICTION: float = 0.15 
+@export var acc: int = 40 #acceleration
+@export var max_speed: int = 100 #max_speed
+
+var move_direction: Vector2 = Vector2.ZERO
+var veloctiy: Vector2 = Vector2.ZERO
+
+#jump - no longer needed at this moment
+const speed = 45
+#const jump_power = -650 
+
+const friction = 60 #friction
+
+#jumping
+const max_jumps = 2
+var current_jumps = 1
+
+#stateMachine
+@onready var state_machine: Node = get_node("FiniteStateMachine")
+
+##health
+#@export var hp: int = 2: set = set_hp
+signal hp_changed(new_hp)
+
+#animated_Sprite
+@onready var animated_sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
+
+#flying
+@export var flying: bool = false
+
+#damage numbers
+#@onready var damage_number_origin: Node2D = get_node("DamageNumbersOrigin")
+
+#death particles in testing
+@export var deathParticle: PackedScene
+
+#loop over and over
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
+	
+#player movement
+func move() -> void:
+	move_direction = move_direction.normalized()
+	velocity += move_direction * acc
+	velocity = velocity.limit_length(max_speed)
+	
+#Taking Damage
+#func take_damage(dam: int, dir: Vector2, force: int) -> void:
+	#if state_machine.state != state_machine.states.hurt and state_machine.state != state_machine.states.dead:
+		#
+		#_spawn_hit_effect() #hit effect
+		#self.hp -= dam #subtracte hp based on damage
+		#
+		##save hp for next level
+		#if name == "player":
+				#SavedData.hp = hp
+				#if hp == 0:
+					#SceneTransistor.start_transition_to("res://scenes/game.tscn")
+					#SavedData.reset_data()
+					#
+		#DamageNumbers.display_number(dam, damage_number_origin.global_position) #display damage numbers
+		##var is_critical self.crit_chance > randf()
+		#
+		##if after taking damage the hp is greater than 0
+		##set the state to hurt and apply a normal knockback (theres no knockbacks in darksouls) are there?
+		##!!!knock back doesnt work - fix later if we want it
+		#
+		##player damaged here
+		#if hp > 0:
+			#state_machine.set_state(state_machine.states.hurt)
+			#velocity += dir * force
+			##print("player hit")
+		#else:
+			#state_machine.set_state(state_machine.states.dead)
+			#velocity += dir * force * 2
+			#
+			##player death here
+			##print("player died")
+			##Kill()
+		
+##called every time we modify the value of the hp variable
+#func set_hp(new_hp: int) -> void:
+	#hp = new_hp
+	#emit_signal("hp_changed", new_hp)
+#
+#func _spawn_hit_effect() -> void:
+	#var hit_effetct: Sprite2D = HIT_EFFECT_SCENE.instantiate()
+	#add_child(hit_effetct)
+	
+func accelerate(direction):
+	velocity = velocity.move_toward(speed * direction, acc)
+	
+func add_friction():
+	velocity = velocity.move_toward(Vector2.ZERO, friction)
+
+#free frame effec
+func frameFreeze(timeScale, duration): #call when you want to freeze "time"
+	Engine.time_scale = timeScale
+	await(get_tree().create_timer(duration * timeScale).timeout)
+	Engine.time_scale = 1.0
+	
