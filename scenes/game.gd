472c45a6ef99f2d5ed8a8b15ec2659f10e2d8190
Isaiah-Mck <Mr.isaiahmck@gmail.com>
2024-05-16 14:05:29 -0700

    Added Shark Spawns

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var fish = preload("res://scenes/fish.tscn")
+var shark = preload("res://scenes/shark.tscn")
 
 func _init() -> void:
 	randomize()
@@ -12,7 +13,15 @@ func _on_timer_timeout():
 	
 	$Player/Path2D/PathFollow2D.progress = rng.randi_range(0,3664)
 	var instance = fish.instantiate()
-	
+
 	instance.global_position = $Player/Path2D/PathFollow2D/Marker2D.global_position
 	add_child(instance)
 
+
+func _on_timer_2_timeout():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	$Player/SharkPath2D/PathFollow2D.progress = rng.randi_range(0,3664)
+	var instance2 = shark.instantiate()
+	instance2.global_position = $Player/SharkPath2D/PathFollow2D/Marker2D.global_position
+	add_child(instance2)
